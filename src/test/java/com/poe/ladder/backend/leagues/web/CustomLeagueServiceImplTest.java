1e66b832092c1cdd5405ef58b0a171d4ab2748ee
David <david.martin@est.tech>
2019-05-22 14:50:48 +0100

    remove unneeded import from CustomLeagueServiceImpl.java

--- src/test/java/com/poe/ladder/backend/leagues/web/CustomLeagueServiceImplTest.java
+++ src/test/java/com/poe/ladder/backend/leagues/web/CustomLeagueServiceImplTest.java
@@ -4,13 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.mockito.ArgumentMatchers.*;
 
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;	
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;

d6f4630a87c6b2851e43ba93d2c7ee17732d698e
David <david.martin@est.tech>
2019-06-10 16:09:53 +0100

    increased the results limit to 100

--- src/main/java/com/poe/ladder/backend/leaderboard/repository/LeaderboardRepository.java
+++ src/main/java/com/poe/ladder/backend/leaderboard/repository/LeaderboardRepository.java
@@ -2,6 +2,7 @@ package com.poe.ladder.backend.leaderboard.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,10 +12,13 @@ import com.poe.ladder.backend.leaderboard.repository.entity.LeaderBoardEntity;
 @Repository
 public interface LeaderboardRepository extends JpaRepository<LeaderBoardEntity, Long> {
 	
+	@Autowired
+	
+	
 	@Query(value="SELECT * FROM leader_board_entity WHERE league=?1 AND leaderboard=?2 LIMIT ?3", nativeQuery = true)
 	List<LeaderBoardEntity> getLeaderboardEntryResults(String leagueVariation, String leaderboard, int limit);
 
-	@Query(value="SELECT * FROM leader_board_entity WHERE league=?1 AND leaderboard=?2 LIMIT 50", nativeQuery = true)
+	@Query(value="SELECT * FROM leader_board_entity WHERE league=?1 AND leaderboard=?2 LIMIT 100", nativeQuery = true)
 	List<LeaderBoardEntity> getLeaderboardLadderResults(String leagueName, String leaderboard);
 	
 }	

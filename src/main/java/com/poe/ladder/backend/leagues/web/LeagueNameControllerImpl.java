347fca98703605be9f892ad75b62bc73af5782d4
David <david.martin@est.tech>
2019-06-10 16:27:58 +0100

    added welcome message entrypoint

--- src/main/java/com/poe/ladder/backend/leagues/web/LeagueNameControllerImpl.java
+++ src/main/java/com/poe/ladder/backend/leagues/web/LeagueNameControllerImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class LeagueNameControllerImpl implements LeagueNameController {
 	
 	@Autowired
 	LeagueNameService leagueNameService;
+	
+	@GetMapping
+	public String rootCheck() {
+		return "Welcome to poe-ladder-do.com";
+	}
 
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/current-leagues")

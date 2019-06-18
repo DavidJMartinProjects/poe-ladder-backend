e0fb5fa580c6c66ab957414dfc4c8a9ccc5a1fa4
David <david.martin@est.tech>
2019-06-18 15:06:07 +0100

    code cleaning and refactoring - renaming variables and classes

--- src/main/java/com/poe/ladder/backend/leaderboard/business/LeagueNameServiceImpl.java
+++ src/main/java/com/poe/ladder/backend/leaderboard/business/LeagueNameServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,46 +18,51 @@ public class LeagueNameServiceImpl implements LeagueNameService {
 	@Autowired
 	LeagueVariationsConfig leagueVariationsConfig;
 	
+	private Map<String, List<String>> leagueVariationMap;
+	
+	@PostConstruct
+	private void init() {
+		leagueVariationMap = leagueVariationsConfig.getLeagueVariations();
+	}
+	
 	@Override	
-	public List<LeagueName> getCurrentLeagues() {		
-		Map<String, List<String>> leagueVariationMap = leagueVariationsConfig.getLeagueVariations();
+	public List<LeagueName> getCurrentLeagues() {				
 		List<LeagueName> leagueNamesList = new ArrayList<>();
 		for (Map.Entry<String, List<String>> leagueEntry : leagueVariationMap.entrySet()) {
 			leagueNamesList.add(new LeagueName(leagueEntry.getKey()));
 		}
-		if (leagueNamesList.isEmpty()) {
-			throw new RuntimeException("getCurrentLeagues() encountered a RuntimeException : League Variations not found for leagueName");
-		}
+		checkListIsNotEmpty(leagueNamesList);
 		return leagueNamesList;
-	}	
+	}
+
 
 	@Override
 	public List<LeagueName> getLeagueVariationNames() {
-		Map<String, List<String>> leagueVariationMap = leagueVariationsConfig.getLeagueVariations();
 		List<LeagueName> leagueVariationsList = new ArrayList<>();
 		for (Map.Entry<String, List<String>> leagueEntry : leagueVariationMap.entrySet()) {
 			for(String leagueVariation : leagueEntry.getValue()) {
 				leagueVariationsList.add(new LeagueName(leagueVariation));
 			}
 		}
-		if (leagueVariationsList.isEmpty()) {
-			throw new RuntimeException("getLeagueVariationNames() encountered a RuntimeException : League Variations not found for leagueName");
-		}
+		checkListIsNotEmpty(leagueVariationsList);
 		return leagueVariationsList;
 	}
 
 	public List<String> getLeagueVariationsListByLeagueName(String leagueName) {
-		Map<String, List<String>> leagueVariationMap = leagueVariationsConfig.getLeagueVariations();
 		List<String> leagueVariationsList = new ArrayList<>();
 		for (Map.Entry<String, List<String>> leagueEntry : leagueVariationMap.entrySet()) {
 			if (leagueEntry.getKey().equals(leagueName)) {
 				leagueVariationsList = leagueEntry.getValue();
 			}
 		}
-		if (leagueVariationsList.isEmpty()) {
-			throw new RuntimeException("getLeagueVariationsListByLeagueName() encountered a RuntimeException  : League Variations not found for leagueName : " + leagueName);
-		}
+		checkListIsNotEmpty(leagueVariationsList);
 		return leagueVariationsList;
 	}
 
+	private void checkListIsNotEmpty(List<?> leagueNamesList) {
+		if (leagueNamesList.isEmpty()) {
+			throw new RuntimeException("RuntimeException : League Variations not found for leagueName");
+		}
+	}	
+	
 }

2b4ffbe764da323d914ac663a027fe303c21f04f
David <david.martin@est.tech>
2019-06-21 09:23:47 +0100

    fix for datatable

--- src/main/java/com/poe/ladder/backend/external/api/response/mapper/LeaderboardMappingUtil.java
+++ src/main/java/com/poe/ladder/backend/external/api/response/mapper/LeaderboardMappingUtil.java
@@ -29,7 +29,7 @@ public class LeaderboardMappingUtil {
 			.leaderboard(leaderboardType.getValue())
 			.rank(responseEntry.getRank().toString())
 			.rankDifference("0")
-			.experienceDifference("0")
+//			.experienceDifference("0")
 			.characterId(responseEntry.getCharacter().getId())
 			.build();
 		
